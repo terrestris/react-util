f2af40aa60ffa71599dc268de5562e23f009f6de
Daniel Koch <koch@terrestris.de>
2023-10-23 17:58:55 +0200

    build: remove unneeded plugins

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,5 @@ module.exports = {
     '@babel/preset-env',
     '@babel/preset-typescript',
     '@babel/preset-react'
-  ],
-  plugins: [
-    '@babel/proposal-class-properties',
-    '@babel/proposal-object-rest-spread',
-    '@babel/proposal-optional-chaining'
   ]
 };
